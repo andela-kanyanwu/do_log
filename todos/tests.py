6fa28f99b3d31df18b8e0397ec8a1a2555cd257e
Kosy Anyanwu <kosy.anyanwu@andela.com>
2017-01-01 22:38:31 +0100

    Set up templates and display categories on home page

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Category
+
+
+class HomeViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+        self.category1 = Category.objects.create(name='category1')
+        self.category2 = Category.objects.create(name='category2')
+        self.category3 = Category.objects.create(name='category3')
+
+    def test_home_page_route(self):
+        response = self.client.get(reverse('todos:home'))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_categories_exist_on_home_page(self):
+        response = self.client.get(reverse('todos:home'))
+
+        self.assertQuerysetEqual(response.context['categories'],
+                                 ['<Category: category1>', '<Category: category2>',
+                                  '<Category: category3>'],
+                                 ordered=False)
--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import Category
+
+
+class HomeView(View):
+
+    def get(self, request):
+        categories = Category.objects.all()
+
+        context = {
+            'categories': categories
+        }
+
+        return render(request, 'todos/home.html', context)
